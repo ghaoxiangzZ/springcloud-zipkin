f25db44c38047e224069bdec804c5806c748e1c8
ghaoxiang <784390929@qq.com>
2019-01-16 15:49:09 +0800

    spring-cloud-zipkin

---
+++ src/main/java/com/ghaoxiang/springcloud/SpringcloudZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.ghaoxiang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudZipkinApplication.class, args);
+	}
+
+}
+
